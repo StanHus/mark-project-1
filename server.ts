5b8be3214460579a7c69a14940462faabfdd8207
Stan <guseletovS@gmail.com>
2021-09-20 14:23:00 +0100

    sabrina's help

--- server.ts
+++ server.ts
@@ -13,9 +13,12 @@ config(); //Read .env file lines as though they were env vars.
 // { rejectUnauthorized: false } - when connecting to a heroku DB
 const herokuSSLSetting = { rejectUnauthorized: false }
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
+//const dataBase_location = process.env.LOCAL ? 'postgres://academy@localhost/project1': 'postgres://ygirkboumscmxk:67d112e9450aaf31271979082c22f1a72fd39b3e0bdcb276c73519e94464658a@ec2-3-220-214-162.compute-1.amazonaws.com:5432/ddi3gefpv87jat'
 const dbConfig = {
+  //connectionString: dataBase_location,
   connectionString: process.env.DATABASE_URL,
   ssl: sslSetting,
+  // ssl: 'postgres://ygirkboumscmxk:67d112e9450aaf31271979082c22f1a72fd39b3e0bdcb276c73519e94464658a@ec2-3-220-214-162.compute-1.amazonaws.com:5432/ddi3gefpv87jat'
 };
 
 const app = express();
@@ -27,10 +30,9 @@ const client = new Client(dbConfig);
 client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
+  const dbres = await client.query('select * from info');
   res.json(dbres.rows);
 });
-
 
 //Start the server on the given port
 const port = process.env.PORT;
